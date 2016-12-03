512eef8499a3ae902e01929b63ad7a48d1af4bd6
hengloose12 <FranklinHeng@Franklins-MacBook-Air.local>
2016-12-03 11:33:30 -0800

    text update

---
+++ Assets/Npc Capsule Collider.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var canvas: Canvas;
+
+function Start () {
+    canvas.enabled = false;
+}
+
+function OnTriggerEnter (info : Collider) {
+    if (info.tag == "Player") {
+        canvas.enabled = true;
+    }
+}
+
+function OnTriggerExit (info : Collider) {
+    if (info.tag == "Player") {
+        canvas.enabled = false;
+    }
+}
